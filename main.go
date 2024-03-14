e13f5073f267a9635137c1ddfbea5c7b1a4c5af0
DmitryStepanov1 <ds.dstepanov@gmail.com>
2024-03-14 13:49:15 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func containsCyrillicOrLatin(s string) bool {
+	for _, r := range s {
+		if unicode.Is(unicode.Cyrillic, r) || unicode.Is(unicode.Latin, r) {
+			return true
+		}
+	}
+	return false
+}
+
+func randomWord(m map[string]string) string {
+	//k := rand.Intn(len(m))
+
+	for i, v := range m {
+		s := fmt.Sprintf("Переведи %s:", i)
+		fmt.Println(s)
+		return v
+	}
+
+	return ""
+
+}
+
+func checkWord(v string) {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+
+		fmt.Print("> ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Check if the user wants to finish entering input
+		if input == "done" {
+			break
+		}
+
+		// Check input value
+		if input == v {
+			fmt.Println("Correct! Try next word")
+			break
+		} else {
+			fmt.Println("Wrong, try again")
+			continue
+		}
+
+	}
+
+}
+
+func main() {
+	// Create a map to store input values
+	inputMap := make(map[string]string)
+
+	// Create a scanner to read input from terminal
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter key-value pairs (key=value) separated by newline. Enter 'done' to finish:")
+
+	// Keep reading input until the user enters 'done'
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Check if the user wants to finish entering input
+		if input == "done" {
+			break
+		}
+
+		// Split input by "=" to separate key and value
+		parts := strings.Split(input, "=")
+		if len(parts) != 2 || len(parts[0]) == 0 || len(parts[1]) == 0 || containsCyrillicOrLatin(parts[0]) == false || containsCyrillicOrLatin(parts[1]) == false {
+			fmt.Println("Invalid input format. Please use 'key=value' format.")
+			continue
+		}
+
+		// Store key-value pair in the map
+		key := parts[0]
+		value := parts[1]
+		inputMap[key] = value
+	}
+
+	for {
+		v := randomWord(inputMap)
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+	jumpTo:
+
+		fmt.Print("> ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Check if the user wants to finish entering input
+		if input == "done" {
+			break
+		} else if input == v {
+			fmt.Println("Correct! Try next word")
+			continue
+		} else {
+			fmt.Println("Wrong, try again")
+			goto jumpTo
+		}
+
+	}
+
+	// Print the map
+	fmt.Println("Map contents:")
+	for key, value := range inputMap {
+		fmt.Printf("%s: %s\n", key, value)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+The simplest flashcards version.
+
+It stores map of words with translations and ask you to fill translations into the terminal. To stop enter "done".
+
+No tests yet.
+
+No refactoring yet.
